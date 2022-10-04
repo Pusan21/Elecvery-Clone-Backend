54838920e8a800a4212dc08f1ffef32915abc0a8
Gilteun Choi <gilteunchoi@gmail.com>
2022-10-05 00:44:47 +0900

    spring boot init

---
+++ src/main/java/com/Pusan21/ElecveryCloneBackend/ElecveryCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Pusan21.ElecveryCloneBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElecveryCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElecveryCloneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Pusan21/ElecveryCloneBackend/ElecveryCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pusan21.ElecveryCloneBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElecveryCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
