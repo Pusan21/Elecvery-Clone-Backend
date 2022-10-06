3af8e08d22d20543ae3454afe0be71bdb027e369
shkisme <shkisme0130@gmail.com>
2022-10-06 22:48:17 +0900

    fix: 로깅 테스트 추가 issue: #2

--- src/main/java/com/Pusan21/ElecveryCloneBackend/controller/PingPongController.java
+++ src/main/java/com/Pusan21/ElecveryCloneBackend/controller/PingPongController.java
@@ -3,9 +3,10 @@ package com.Pusan21.ElecveryCloneBackend.controller;
 import com.Pusan21.ElecveryCloneBackend.dto.PingPongDto;
 import com.Pusan21.ElecveryCloneBackend.service.PingPongService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class PingPongController {
@@ -13,6 +14,8 @@ public class PingPongController {
 
   @GetMapping("/ping")
   public PingPongDto pingPong() {
-    return pingPongService.pong();
+    PingPongDto pong = pingPongService.pong();
+    log.info(pong.toString());
+    return pong;
   }
 }
