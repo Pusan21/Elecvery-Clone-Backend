494ef40f51f5a570d9ad931ea5f1418ed7e25c1c
shkisme <shkisme0130@gmail.com>
2022-11-12 15:52:32 +0900

    fix: Spring Security 설정에서 csrf 설정 비활성화 (테스트를 돌렸을 때 403 Forbidden 에러 발생 방지) issue: #37

--- src/main/java/com/Pusan21/ElecveryCloneBackend/config/SecurityConfiguration.java
+++ src/main/java/com/Pusan21/ElecveryCloneBackend/config/SecurityConfiguration.java
@@ -3,8 +3,12 @@ package com.Pusan21.ElecveryCloneBackend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,7 +21,10 @@ public class SecurityConfiguration {
     http.authorizeRequests(authorize -> authorize
         .antMatchers(HttpMethod.OPTIONS).permitAll()
         .anyRequest().permitAll()
+
     );
+    // TODO: Spring Security 기능이 완성되면 바꿔야 함.
+    http.csrf().disable();
 
     return http.build();
   }
